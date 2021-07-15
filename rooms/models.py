a63ec7a574ca016f21c6e4dc56c3a898537e72f9
wnsgud0428 <wnsgud0428@naver.com>
2021-07-15 22:16:49 +0900

    #4.4 on_delete, Amenity, Faciliy, HouseRule Models

--- rooms/models.py
+++ rooms/models.py
@@ -6,6 +6,43 @@ from users import models as user_models
 # Create your models here.
 
 
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """RoomType Model Definition"""
+
+    pass
+
+
+class Amenity(AbstractItem):
+    """Amenity Model Definition"""
+
+    pass
+
+
+class Facility(AbstractItem):
+    """Facility Model Definition"""
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """HouseRule Model Definition"""
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definition"""
@@ -24,3 +61,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
+
+    def __str__(self):
+        return self.name
