1aa8b595301b258a1fb2ed812ea8d0521fd02483
wnsgud0428 <wnsgud0428@naver.com>
2021-07-22 22:02:06 +0900

    #7.2 Many to Many _sets

--- reviews/models.py
+++ reviews/models.py
@@ -12,10 +12,12 @@ class Review(core_models.TimeStampedModel):
     location = models.IntegerField()
     check_in = models.IntegerField()
     value = models.IntegerField()
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    # user 삭제되면 review도 삭제(cascade)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
-    # room 이 삭제되면 review도 삭제(cascade)
+    user = models.ForeignKey(
+        "users.User", related_name="reviews", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reviews", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.review} - {self.room}"
