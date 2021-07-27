5fd9013548f62ed62cffd4ba0ac434abe93f1900
wnsgud0428 <wnsgud0428@naver.com>
2021-07-27 15:21:27 +0900

    #9.5 seed_rooms part Three

--- rooms/admin.py
+++ rooms/admin.py
@@ -29,7 +29,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "city", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price", "room_type")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
@@ -76,10 +76,12 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+    count_amenities.short_description = "Amenity Count"
+        
 
     def count_photos(self, obj):
         return obj.photos.count()
-
+    count_photos.short_description = "Photo Count"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
