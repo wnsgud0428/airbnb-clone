378983ee4c04d103b04463270f441f35a8140c98
wnsgud0428 <wnsgud0428@naver.com>
2021-08-02 22:16:54 +0900

    #12.3 Http404()

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "shrxhq^$$_3*f5h73fp9)0o&p6j2+h&pm2b88kg-xs)qb()687"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
