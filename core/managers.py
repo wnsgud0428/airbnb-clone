ec75b49bb7e1a680d928a99bae1a2845a73a8ce6
wnsgud0428 <wnsgud0428@naver.com>
2022-01-18 22:48:32 +0900

    #25.8 Conversation Styles

--- core/managers.py
+++ core/managers.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
 class CustomModelManager(models.Manager):
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+
+class CustomModelManager2(UserManager):
     def get_or_none(self, **kwargs):
         try:
             return self.get(**kwargs)
